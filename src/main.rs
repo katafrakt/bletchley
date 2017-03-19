ba71a71568a2ac8a85a821e89f9948e71bfc2774
Paweł Świątkowski <pawel.swiatkowski@boostcom.no>
2017-03-19 10:40:18 +0100

    Add clap to parse CL params

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+#[macro_use]
+extern crate clap;
 mod crypto;
 use crypto::key_generator;
 
 fn main() {
-    let pair = key_generator::generate_pair("rsa");
-    match pair {
-        Some(p) => p.write_keys("test"),
-        None => print!("Could not generate keys")
+    let matches = clap_app!(bletchley =>
+        (version: "0.1")
+        (author: "Paweł Świątkowski")
+        (@subcommand gen =>
+            (about: "Generates keys")
+            (@arg output: -o --output +takes_value "Base of the filenames of created keys (default: \"id\")")
+        )
+    ).get_matches();
+
+
+    if let Some(matches) = matches.subcommand_matches("gen") {
+        let name = matches.value_of("output").unwrap_or("id");
+
+        let pair = key_generator::generate_pair("rsa");
+        match pair {
+            Some(p) => p.write_keys(name),
+            None => print!("Could not generate keys")
+        }
     }
 }
